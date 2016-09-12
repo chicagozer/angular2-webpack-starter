48223fe8fac14582b7ef6495f05a79c0f06910d0
Jim Mittler <jim.mittler@xtime.com>
2016-09-11 17:35:06 -0700

    latest

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -21,7 +21,7 @@ const AssetsPlugin = require('assets-webpack-plugin');
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
   title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
-  baseUrl: '/',
+  baseUrl: '/angular2-webpack-starter',
   isDevServer: helpers.isWebpackDevServer()
 };
 
